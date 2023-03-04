2911abd32fa78bec1ac63aba7381abc123efc582
MK-09-coder <110844185+MK-09-coder@users.noreply.github.com>
2023-03-04 18:07:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Participant, Profile , Firstpage,Wrong,Exit,Correct,Rankings } from './components';
+import {Home,Creator,Game,Join,Instructions, GetReady, Login} from "./pages"
+function App() {
+  return (
+    <div className="App">
+     <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/game" element={<Game />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/instructions" element={<Instructions />} />
+          <Route path= "/getReady" element={<GetReady />} />
+          <Route path='/Participant' element={<Participant/>}/>
+          <Route path="/Wrong" element={<Wrong/>} />
+          <Route path="/Correct" element = {<Correct/>}/>
+          <Route path="/Rankings" element = {<Rankings/>}/>
+        </Routes>
+        <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/creator" element={<Creator />} />
+        <Route path="/Firstpage" element={<Firstpage/>}/>
+        <Route path="/Exit" element={<Exit/>}/>
+        <Route path="/Profile" element={<Profile/>}/>
+        </Routes>
+     </Router>
+    </div>
+  );
+}
+
+export default App;
